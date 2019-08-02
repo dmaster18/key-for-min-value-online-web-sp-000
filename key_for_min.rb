333d34977eb4546f19c086de241243a32dc89ac7
David Strauch <dmaster18@gmail.com>
2019-08-02 15:37:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -24,6 +24,8 @@ end
 def key_for_min_value(name_hash)
   name_of_key = ""
   smallest_value = smallest_value(name_hash)
+  i = 0 
+  
   name_hash.find do |key, value|
     if value == smallest_value
       name_of_key = key

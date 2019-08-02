f34cdc5e78a5f622b404530ca855101b8450bd76
David Strauch <dmaster18@gmail.com>
2019-08-02 15:31:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -28,7 +28,7 @@ def key_for_min_value(name_hash)
     if value == smallest_value
       name_of_key = key
     elsif value == ""
-      name_of_key = 
+      name_of_key = nil
     end
   end
   name_of_key
